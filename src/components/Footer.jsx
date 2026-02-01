1138808498b2172e0372540f48853f4f16a1cf84
Víctor <victor.gonzalezsalas.bcn@gmail.com>
2026-02-01 11:17:13 +0100

    Footer con enlaces

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,12 +11,12 @@ function Footer() {
         color: "#94a3b8",
       }}
     >
-      <p>© {new Date().getFullYear()} TuNombre.dev. Todos los derechos reservados.</p>
+      <p>© {new Date().getFullYear()} victorgonzalezsalas.dev. Todos los derechos reservados.</p>
       <div style={{ marginTop: "10px", display: "flex", justifyContent: "center", gap: "15px" }}>
-        <a href="https://github.com/tuusuario" target="_blank" rel="noopener noreferrer" style={{ color: "#38bdf8" }}>
+        <a href="https://github.com/victorgonzalezsalasbcn-ctrl" target="_blank" rel="noopener noreferrer" style={{ color: "#38bdf8" }}>
           GitHub
         </a>
-        <a href="https://linkedin.com/in/tuusuario" target="_blank" rel="noopener noreferrer" style={{ color: "#38bdf8" }}>
+        <a href="https://www.linkedin.com/in/víctor-gonzález-salas-07a941163/" target="_blank" rel="noopener noreferrer" style={{ color: "#38bdf8" }}>
           LinkedIn
         </a>
       </div>
